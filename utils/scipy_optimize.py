9afdf440773d7e323cb186e3c63119a8c972ec6f
Kamil Celejewski <44023880+kcc112@users.noreply.github.com>
2021-03-22 19:58:24 +0100

    Update scipy_optimize.py

--- utils/scipy_optimize.py
+++ utils/scipy_optimize.py
@@ -9,4 +9,4 @@ def scipy_optimize(fun, interval_start, interval_end, epsilon, iter_count):
     result = optimize.minimize_scalar(f, bounds=(interval_start, interval_end), method='bounded', options={'maxiter': iter_count, 'xatol': epsilon})
     minimum = result.x
     print(f'Minimum wg. scipy: {minimum}')
-    return minimum;
+    return minimum
